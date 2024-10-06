b0bafd48687c2821a8a4eac38e1ec8e32bb0fc73
mohitanand744 <jhamohit744@gmail.com>
2024-10-06 16:29:21 +0530

    Profile UI is Complete

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -3,9 +3,9 @@ import Button from "../common/Buttons/Button";
 
 const Home = () => {
   return (
-    <div className="hero px-2 md:px-10 mt-36 lg:mt-52">
-      <div className="row align-items-center justify-content-center">
-        <div className="col-lg-7 col-md-12 col-12 h-[48vh] order-2 lg:order-1">
+    <div className="hero px-3 md:px-10 mt-36 lg:mt-52">
+      <div className="grid gap-10 grid-cols-1 lg:grid-cols-12">
+        <div className="lg:col-span-7 h-[48vh] order-2 lg:order-1">
           <div className="flex justify-center items-center gap-3 h-full ">
             <ul className="list-none flex flex-col gap-2">
               <li>
@@ -32,10 +32,10 @@ const Home = () => {
             </ul>
 
             <div className="head">
-              <h1 className="font-bold text-white text-[2.3rem] sm:text-[3rem] md:text-[5rem]">
+              <h1 className="font-bold text-white text-[2.6rem] sm:text-[3rem] md:text-[5rem]">
                 Hi There, I'm <span className="highLight-text">Mohit</span>{" "}
                 <br />{" "}
-                <span className="text-[1.8rem] sm:text-[2.3rem] md:text-[3.8rem]">
+                <span className="text-[2rem] sm:text-[2.3rem] md:text-[3.8rem]">
                   {" "}
                   a <span className="highLight-text">Web Developer</span>
                 </span>
@@ -51,9 +51,9 @@ const Home = () => {
             </div>
           </div>
         </div>
-        <div className="col-lg-5 col-md-12 col-12 order-1 lg:order-2">
+        <div className="lg:col-span-5 order-1 lg:order-2">
           <div className="flex justify-center items-center ">
-            <div className="ms-2 profileImageContainer w-[28rem] md:w-[35rem] h-[28rem] md:h-[35rem] rounded-full">
+            <div className="ms-2 profileImageContainer w-[25rem] md:w-[35rem] h-[25rem] md:h-[35rem] rounded-full">
               <img
                 className="w-full h-full rounded-full object-cover"
                 src="/hero-profile.jpeg"
