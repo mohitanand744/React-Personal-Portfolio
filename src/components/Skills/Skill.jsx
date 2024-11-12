0f6854def1842c6371f9631248d3c41958a59e28
mohitanand744 <jhamohit744@gmail.com>
2024-11-12 19:41:52 +0530

    UI Changes

--- src/components/Skills/Skill.jsx
+++ src/components/Skills/Skill.jsx
@@ -14,11 +14,11 @@ const Skill = () => {
     <div
       id="Skills"
       data-aos="zoom-in"
-      className="skills h-fit mt-32 md:my-44 px-3 lg:px-10"
+      className="px-3 mt-32 skills h-fit md:my-44 lg:px-10"
     >
       <center className="my-10 mx-auto w-[96%] lg:w-[40%] mb-14">
         <SectionHeading text={"Skills"} />
-        <p className="text-white text-xl lg:text-2xl ">
+        <p className="text-xl text-white lg:text-2xl ">
           Expertise in multiple frameworks and a passion for continuous
           learning, seeking to expand my skills as a full-stack developer.
         </p>
@@ -57,12 +57,12 @@ const Skill = () => {
         </button>
       </div>
 
-      <div className="w-[97%] md:w-[78%] lg:w-[60%] mx-auto flex rounded-3xl my-16 gap-14 justify-center flex-wrap  items-center">
+      <div className="w-[98%] md:w-[78%] lg:w-[85%] mx-auto flex rounded-3xl my-16 gap-14 justify-center flex-wrap  items-center">
         {filterSkillsCategory.map((skill, i) => (
           <div
             key={i}
             data-aos="zoom-in"
-            className="flex flex-col gap-3 justify-center items-center text-xl md:text-2xl font-bold"
+            className="flex flex-col justify-center items-center bg-[#00000081] w-[14rem] md:w-[14rem] lg:w-[18rem] py-5 rounded-3xl text-xl md:text-2xl font-bold custom-shadow"
           >
             <img
               loading="lazy"
@@ -70,7 +70,7 @@ const Skill = () => {
               src={skill.image}
               alt=""
             />
-            <p className="text-white text-lg sm:text-xl">{skill.title}</p>
+            <p className="text-lg text-white sm:text-xl">{skill.title}</p>
           </div>
         ))}
       </div>
