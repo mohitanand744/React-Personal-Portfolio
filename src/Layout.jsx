7427132c4e0c63b2641fdd28cbb087918da8bcdb
mohitanand744 <jhamohit744@gmail.com>
2024-11-18 16:21:20 +0530

    Optimization: Used Lazy Loading

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,31 +1,16 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Nav from "./components/common/Nav/Nav";
 import { Outlet } from "react-router-dom";
 import StoreProvider from "./context/Store";
-import Loading from "./components/Loadings/Loading";
 import Footer from "./components/common/Footers/Footer";
 
 const Layout = () => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 3000);
-  }, []);
-
   return (
     <div>
       <StoreProvider>
-        {loading ? (
-          <Loading />
-        ) : (
-          <>
-            <Nav />
-            <Outlet />
-            <Footer />
-          </>
-        )}
+        <Nav />
+        <Outlet />
+        <Footer />
       </StoreProvider>
     </div>
   );
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -6,18 +6,26 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import App from "../App";
-import Layout from "./../Layout";
+
 import Error404 from "../Pages/Error404";
 
 import { lazy } from "react";
 import Loading from "../components/Loadings/Loading";
 
+const Layout = lazy(() => import("./../Layout"));
 const AllProjects = lazy(() => import("../Pages/AllProjects"));
 
 const Router = () => {
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<Layout />}>
+      <Route
+        path="/"
+        element={
+          <Suspense fallback={<Loading />}>
+            <Layout />
+          </Suspense>
+        }
+      >
         <Route index element={<App />} />
         <Route
           path="projects"
